cbb70999b9bffd7a0e8bb7a4f4c2ecb02c44ec75
Alin Bodea <cinewva@gmail.com>
2024-05-08 15:13:35 +0300

    I translated into english

--- src/com/itfactory/Main.java
+++ src/com/itfactory/Main.java
@@ -8,15 +8,15 @@ public class Main {
         // The code below initializes an object to read input from the keyboard.
         Scanner scanner = new Scanner(System.in);
 
-        // Display a message asking for text input from the keyboard.
-        // The text should be entered followed by pressing the Enter key.
+// Display a message asking for text input from the keyboard.
+// The text should be entered followed by pressing the Enter key.
         System.out.print("Enter your name: ");
 
-        // The line of code that will actually read the text from the keyboard.
-        // Define a variable of type String to store the entered text.
+// The line of code that will actually read the text from the keyboard.
+// Define a variable of type String to store the entered text.
         String personName = scanner.nextLine();
 
-        // Display the name entered by the user.
+// Display the name entered by the user.
         System.out.println("The entered name is: " + personName);
 
 
